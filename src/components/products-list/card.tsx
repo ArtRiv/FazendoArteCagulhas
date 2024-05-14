d68a8a37f4ce4d03adc8521699d1f7fb7dc0c9ca
unknown <arthurfelaco707@gmail.com>
2024-05-14 20:16:42 -0300

    Fixed hydration error

--- src/components/products-list/card.tsx
+++ src/components/products-list/card.tsx
@@ -2,8 +2,9 @@ import { formatPrice } from "@/utils/formatPrice";
 import { Product } from "@/types/product";
 import Image from "next/image";
 import { AnchorClientSide } from "../ui/anchor-client-side";
-import { CardVariants } from "@/types/cardVariants";
+import { CardVariants } from "@/types/component-variants/card-variants";
 import { StarsRating } from "../ui/stars-rating";
+import Link from "next/link";
 
 export const ProductCard = ({
     productData,
@@ -18,54 +19,47 @@ export const ProductCard = ({
         <>
             <li className="flex flex-grow h-auto cursor-pointer grid--item">
                 <div className="w-60 h-full rounded-radius-normal bg-transparent relative no-underline animateCard shadow-xl">
-                    <AnchorClientSide
-                        navigateLink={`/product?id=${productData.id}`}
-                        twStyles="absolute top-0 left-0 w-full h-full z-[3] slide-in slideInEffect"
-                    >
-                        <div></div>
-                    </AnchorClientSide>
-                    <Image
-                        className={`${variant === CardVariants.NORMAL ? 'shape--blob' : 'rounded-t-xl'} image`}
-                        alt={productData.title}
-                        src={productData.image}
-                        width={300}
-                        height={300}
-                        quality={100}
-                        loading="lazy"
-                        draggable={false}
-                    />
-                    <div className={`flex flex-col ${variant === CardVariants.NORMAL ? 'h-52' : ''} pt-0 pr-2 pb-2 pl-2`}>
-                        <div className="flex flex-col flex-1 h-36 px-2 py-3">
-                            <h3 className="h-11 text-big text-font-color font-harmonia leading-line-height-normal tracking-letter-space-normal break-words antialiased">
-                                {productData.title}
-                            </h3>
-                            <span className="text-small text-font-color/50 font-harmonia leading-line-height-small tracking-letter-space-normal break-words antialiased">
-                                amigurumi
-                            </span>
-                            {productData.rating > 0 && (
-                                <StarsRating stars={productData.rating} purchase_count={productData.purchase_count} />
-                            )}
-                            {productData.rating == 0 && (
-                                <StarsRating stars={0} purchase_count={productData.purchase_count} />
-                            )}
-                            <h4 className={`flex items-end ${variant === CardVariants.NORMAL ? 'flex-1' : ''} text-normal text-font-color/80 font-harmonia leading-line-height-big tracking-letter-space-normal break-words antialiased`}>
-                                {price}
-                            </h4>
-                        </div>
-                        {variant === CardVariants.NORMAL &&
-                            <AnchorClientSide
-                                navigateLink={`/product?id=${productData.id}`}
-                                twStyles="inline-flex items-center justify-center relative z-[4] px-3 py-2 bg-background border-2 border-solid border-decoration-indigo rounded-radius-normal cursor-pointer
-                                [box-shadow:0_2px_14px_0_rgba(0,_0,_0,_0.183)]
-                                [transition:transform_0.5s_ease,_box-shadow_0.5s_ease]
-                                hover:shadow-xl hover:origin-center hover:[transform:box-shadow_0.5s_ease]
-                                cardButton">
-                                <span className="bg-backg text-small text-font-color font-harmonia leading-line-height-big tracking-letter-space-normal break-words antialiased">
-                                    Ver produto
+                    <Link href={`/product?id=${productData.id}`} className="slide-in slideInEffect">
+                        <Image
+                            className={`${variant === CardVariants.NORMAL ? 'shape--blob' : 'rounded-t-xl'}`}
+                            alt={productData.title}
+                            src={productData.image}
+                            width={240}
+                            height={240}
+                            quality={100}
+                            loading="lazy"
+                            draggable={false}
+                        />
+                        <div className={`flex flex-col ${variant === CardVariants.NORMAL ? 'h-52' : ''} pt-0 pr-2 pb-2 pl-2`}>
+                            <div className="flex flex-col flex-1 h-36 px-2 py-3">
+                                <h3 className="h-11 text-big text-font-color font-harmonia leading-line-height-normal tracking-letter-space-normal break-words antialiased">
+                                    {productData.title}
+                                </h3>
+                                <span className="text-small text-font-color/50 font-harmonia leading-line-height-small tracking-letter-space-normal break-words antialiased">
+                                    amigurumi
                                 </span>
-                            </AnchorClientSide>
-                        }
-                    </div>
+                                {productData.rating > 0 && (
+                                    <StarsRating stars={productData.rating} purchase_count={productData.purchase_count} />
+                                )}
+                                {productData.rating == 0 && (
+                                    <StarsRating stars={0} purchase_count={productData.purchase_count} />
+                                )}
+                                <h4 className={`flex items-end ${variant === CardVariants.NORMAL ? 'flex-1' : ''} text-normal text-font-color/80 font-harmonia leading-line-height-big tracking-letter-space-normal break-words antialiased`}>
+                                    {price}
+                                </h4>
+                            </div>
+                            {variant === CardVariants.NORMAL &&
+                                <div className="inline-flex items-center justify-center relative z-[4] px-3 py-2 bg-background border-[1.5px] border-solid border-decoration-indigo rounded-radius-normal cursor-pointer
+                                shadow-md [transition:box-shadow_0.5s_ease]
+                                hover:shadow-xl hover:origin-center
+                                cardButton">
+                                    <span className="bg-backg text-small text-font-color font-harmonia leading-line-height-big tracking-letter-space-normal break-words antialiased">
+                                        Ver produto
+                                    </span>
+                                </div>
+                            }
+                        </div>
+                    </Link>
                 </div>
             </li>
         </>
