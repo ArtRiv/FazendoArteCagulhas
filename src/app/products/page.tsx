34e4e997973e9aff2ececde2f4adb64fddcf39b5
unknown <arthurfelaco707@gmail.com>
2024-06-10 18:39:48 -0300

    refactor: change to not be a dynamic route and now use backend api fetching

---
+++ src/app/products/page.tsx
@@ -0,0 +1,41 @@
+import { getCollectionParams } from "@/types/productParams";
+import { DefaultPageLayout } from "@/components/default-page-layout";
+import { ProductsPagination } from "@/app/products/_components/pagination";
+import { ProductsList } from "@/components/products-list";
+import { FilterBar } from "@/components/filter-bar.tsx";
+import useQueryParams from "@/utils/settings/getServerSettings";
+import { Hero } from "@/app/products/_components/hero";
+import { CardVariants } from "@/types/component-variants/card-variants";
+import { getCollectionData } from "@/services/collection";
+
+export default async function ProductsPage() {
+
+    const { category_id, sort_by, page, pageSize } = useQueryParams();
+
+    const params: getCollectionParams = {
+        page,
+        category_id,
+        sort_by,
+    };
+    const productsData = await getCollectionData(params);
+
+    return (
+        <DefaultPageLayout>
+            {(productsData) &&
+                <>
+                    <Hero />
+                    <FilterBar />
+                    <ProductsList productsData={productsData} cardVariant={CardVariants.NORMAL}/>
+                    <ProductsPagination
+                    productsCount={productsData.length}
+                    pageSize={pageSize} />
+                </>
+            }
+            {(!productsData) &&
+                <div className="w-auto mx-auto my-0 p-12 flex justify-center">
+                    hmm... parece que algo deu errado (╥﹏╥)
+                </div>
+            }
+        </DefaultPageLayout>
+    );
+}
