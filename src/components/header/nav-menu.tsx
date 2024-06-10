6968ab4c900c9a84353515fb677a94c37aa590fb
unknown <arthurfelaco707@gmail.com>
2024-06-10 18:40:55 -0300

    refactor: fetch categories from backend api and map over it instead of hardcoding

--- src/components/header/nav-menu.tsx
+++ src/components/header/nav-menu.tsx
@@ -1,11 +1,29 @@
 import { AnchorClientSide } from "@/components/ui/anchor-client-side";
 import { StaggeredDropDown, OptionWithLink } from "../ui/dropdown";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { DropdownVariants } from "@/types/component-variants/dropdown-variants";
+import { Categories } from "@/types/categories";
+import { getCategories } from "@/services/categories";
+import { motion } from "framer-motion";
+import { FiChevronDown } from "react-icons/fi";
 
 export const HeaderNavigationMenu = () => {
 
     const [open, setOpen] = useState(false);
+    const [categories, setCategories] = useState<Categories[]>();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await getCategories();
+                setCategories(response);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     return (
         <nav className="[grid-area:navigation]">
@@ -26,15 +44,23 @@ export const HeaderNavigationMenu = () => {
 
                 <li>
                     <div className="inline-flex justify-self-end p-3">
-                        <StaggeredDropDown variant={DropdownVariants.LINK} text="Produtos">
-                            <OptionWithLink setOpen={setOpen} text="Todos" navigateLink="/products/all" />
-                            <OptionWithLink setOpen={setOpen} text="Animes" navigateLink="/products/animes" />
-                            <OptionWithLink setOpen={setOpen} text="Games" navigateLink="/products/games" />
-                            <OptionWithLink setOpen={setOpen} text="Geek" navigateLink="/products/geek" />
-                            <OptionWithLink setOpen={setOpen} text="Fofo" navigateLink="/products/cute" />
-                            <OptionWithLink setOpen={setOpen} text="Religioso" navigateLink="/products/religious" />
-                            <OptionWithLink setOpen={setOpen} text="Pelúcias" navigateLink="/products/plushies" />
-                        </StaggeredDropDown>
+                        {categories && (
+                            <StaggeredDropDown variant={DropdownVariants.LINK} text="Produtos">
+                                {categories?.map(category => {
+                                    return (
+                                        <OptionWithLink key={category.id} setOpen={setOpen} text={category.name.toLowerCase()} navigateLink={`/products?category_id=${category.id}`} />
+                                    )
+                                })}
+                            </StaggeredDropDown>
+                        )}
+                        {!categories && (
+                            <div className="flex gap-2">
+                                <span className="relative font-harmonia text-normal text-font-color leading-line-height-small tracking-letter-space-small select-none underline [text-decoration-color:transparent] hover:transition-all animateBorderBottom changeTextColor">
+                                    Produtos
+                                </span>
+                                <FiChevronDown />
+                            </div>
+                        )}
                     </div>
                 </li>
 
