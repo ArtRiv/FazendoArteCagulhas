ac4a55b859e206c262d83300cfe74ca3991a49bc
unknown <arthurfelaco707@gmail.com>
2024-06-10 18:42:07 -0300

    feat: add function to fetch products from backend

---
+++ src/services/collection/index.ts
@@ -0,0 +1,19 @@
+import { Product } from "@/types/product";
+import { getCollectionParams } from "@/types/productParams";
+
+export async function getCollectionData(params: getCollectionParams): Promise<Product[]>{
+    
+    const options = {
+        method: 'GET',
+    };
+    const url = `http://localhost:8080/collection?category_id=${params.category_id}&sort_by=${params.sort_by}&page=${params.page}`;
+    const res = await fetch(url, options);
+
+    if(!res.ok) {
+        console.log('erro');
+    }
+
+    const data = await res.json(); 
+
+    return data;
+}
