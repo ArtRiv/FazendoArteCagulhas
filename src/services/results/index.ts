c85f20f512f7de749efc9c3a6f8753ff32759938
unknown <arthurfelaco707@gmail.com>
2024-06-11 16:06:14 -0300

    feat: create function to fetch results

---
+++ src/services/results/index.ts
@@ -0,0 +1,18 @@
+import { Product } from "@/types/product";
+import { getProductsBySearchParams } from "@/types/productParams";
+
+export async function getResults(params: getProductsBySearchParams): Promise<Product[]>{
+    const options = {
+        method: 'GET',
+    };
+    const url = `http://localhost:8080/results?search_query=${params.search_query}&sort_by=${params.sort_by}&page=${params.page}`;
+    const res = await fetch(url, options);
+
+    if(!res.ok) {
+        console.log('erro');
+    }
+
+    const data = await res.json(); 
+
+    return data;
+}
