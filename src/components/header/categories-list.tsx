c8fc6e316391f4420a28a8b3a684b84e7e6aba16
unknown <arthurfelaco707@gmail.com>
2024-06-13 18:03:07 -0300

    refactor: separate categories list into a component

---
+++ src/components/header/categories-list.tsx
@@ -0,0 +1,54 @@
+import { getCategories } from "@/services/categories"
+import { Categories } from "@/types/categories"
+import { DropdownVariants } from "@/types/component-variants/dropdown-variants";
+import { useCallback, useEffect, useState } from "react"
+import { FiChevronDown } from "react-icons/fi";
+import { StaggeredDropDown, OptionWithLink } from "../ui/dropdown";
+
+export const CategoriesList = () => {
+    const [categories, setCategories] = useState<Categories[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    const fetchData = useCallback(async () => {
+        try {
+            const response = await getCategories();
+            setCategories(response);
+        } catch (error) {
+            setError((error as Error).message);
+        } finally {
+            setIsLoading(false);
+        }
+    }, []);
+
+    useEffect(() => {
+        fetchData();
+    }, [fetchData]);
+
+    return (
+        <div className="inline-flex justify-self-end p-3">
+            {(categories && !isLoading) && (
+                <StaggeredDropDown variant={DropdownVariants.LINK} text="Produtos">
+                    {categories?.map(category => {
+                        return (
+                            <OptionWithLink key={category.id} text={category.name.toLowerCase()} navigateLink={`/products?category_id=${category.id}`} />
+                        )
+                    })}
+                </StaggeredDropDown>
+            )}
+            {isLoading && (
+                <div className="flex gap-2">
+                    <span className="relative font-harmonia text-normal text-font-color leading-line-height-small tracking-letter-space-small select-none underline [text-decoration-color:transparent] hover:transition-all animateBorderBottom changeTextColor">
+                        Produtos
+                    </span>
+                    <FiChevronDown />
+                </div>
+            )}
+            {error && (
+                <div>
+                    Erro
+                </div>
+            )}
+        </div>
+    )
+}
--- src/components/header/nav-menu.tsx
+++ src/components/header/nav-menu.tsx
@@ -4,26 +4,12 @@ import { useEffect, useState } from "react";
 import { DropdownVariants } from "@/types/component-variants/dropdown-variants";
 import { Categories } from "@/types/categories";
 import { getCategories } from "@/services/categories";
-import { motion } from "framer-motion";
 import { FiChevronDown } from "react-icons/fi";
+import { CategoriesList } from "./categories-list";
 
 export const HeaderNavigationMenu = () => {
 
     const [open, setOpen] = useState(false);
-    const [categories, setCategories] = useState<Categories[]>();
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await getCategories();
-                setCategories(response);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-
-        fetchData();
-    }, []);
 
     return (
         <nav className="[grid-area:navigation]">
@@ -43,25 +29,7 @@ export const HeaderNavigationMenu = () => {
                 </li>
 
                 <li>
-                    <div className="inline-flex justify-self-end p-3">
-                        {categories && (
-                            <StaggeredDropDown variant={DropdownVariants.LINK} text="Produtos">
-                                {categories?.map(category => {
-                                    return (
-                                        <OptionWithLink key={category.id} setOpen={setOpen} text={category.name.toLowerCase()} navigateLink={`/products?category_id=${category.id}`} />
-                                    )
-                                })}
-                            </StaggeredDropDown>
-                        )}
-                        {!categories && (
-                            <div className="flex gap-2">
-                                <span className="relative font-harmonia text-normal text-font-color leading-line-height-small tracking-letter-space-small select-none underline [text-decoration-color:transparent] hover:transition-all animateBorderBottom changeTextColor">
-                                    Produtos
-                                </span>
-                                <FiChevronDown />
-                            </div>
-                        )}
-                    </div>
+                    <CategoriesList/>
                 </li>
 
                 <li>
