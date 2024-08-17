a09ec3be1dd7575db0493e22e5c95d1fd86d01c2
ArtRiv <arthurfelaco707@gmail.com>
2024-08-17 18:43:49 -0300

    feat: add logic to render next payment flow step

--- src/components/header/components/cart/payment-flow/index.tsx
+++ src/components/header/components/cart/payment-flow/index.tsx
@@ -1,51 +1,36 @@
-import { Elements } from '@stripe/react-stripe-js';
-import { DialogDemo } from '../teste';
 import AddressStep from './address-step';
-import { loadStripe } from '@stripe/stripe-js';
 import { useState } from 'react';
-import { Dialog, DialogContent, DialogHeader, DialogTrigger } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import { BreadcrumbResponsive } from './address-step/breadcrumb';
-
-// Make sure to call `loadStripe` outside of a component’s render to avoid
-// recreating the `Stripe` object on every render.
-const stripePromise = loadStripe(
-    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
-);
-
-const options = {
-    appearance: {
-        rules: {
-            '.Input': {
-                borderColor: '#ff23b6', // Input border color
-                boxShadow: 'none', // Remove default shadow
-            },
-            '.Input--focus': {
-                borderColor: '#0070f3', // Border color on focus
-            },
-        },
-    },
-};
+import PaymentStep from './payment-step';
 
 export const CheckoutFlow = () => {
-    const [showCheckout, setShowCheckout] = useState(false);
+    const [currentStep, setCurrentStep] = useState(1);
 
-    const handleCheckoutClick = () => {
-        setShowCheckout(true);
+    const handleNextStep = () => {
+        setCurrentStep((prevStep) => prevStep + 1);
+    }
+
+    const renderStep = () => {
+        switch (currentStep) {
+            case 1:
+                return <AddressStep onNext={handleNextStep} />
+            case 2:
+                return <PaymentStep />
+            default:
+                return null;
+        }
     };
 
     return (
         <Dialog>
-            <DialogTrigger asChild onClick={handleCheckoutClick}>
+            <DialogTrigger asChild>
                 <Button variant="outline">Pagar</Button>
             </DialogTrigger>
             <DialogContent className="sm:max-w-[425px] md:max-w-[625px] lg:max-w-[1125px] overflow-y-scroll h-[95%]">
                 <BreadcrumbResponsive />
-                {showCheckout && (
-                    <Elements stripe={stripePromise} options={options}>
-                        <AddressStep />
-                    </Elements>
-                )}
+                {renderStep()}
             </DialogContent>
         </Dialog>
     );
