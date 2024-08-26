3b522b9670f2f53797f3daa6295f8cfff4a31494
ArtRiv <arthurfelaco707@gmail.com>
2024-08-26 17:07:59 -0300

    feat: add disabled state for address form button

--- src/components/header/components/cart/payment-flow/address-step/index.tsx
+++ src/components/header/components/cart/payment-flow/address-step/index.tsx
@@ -4,10 +4,12 @@ import { ShippingOptions } from "./shipping-options";
 import { FormProps } from "@/types/form-type-props";
 import { SubmitHandler } from "react-hook-form";
 import { Button } from "@/components/ui/button";
+import { useFilter } from "@/hooks/use-filter";
 
 export default function AddressStep({ onNext }: { onNext: () => void }) {
     const [showShippingOptions, setShowShippingOptions] = useState(false);
     const [formData, setFormData] = useState<FormProps>();
+    const {selectedShippingOption} = useFilter();
 
     const onSubmit: SubmitHandler<FormProps> = (data) => {
         setShowShippingOptions(true);
@@ -28,7 +30,12 @@ export default function AddressStep({ onNext }: { onNext: () => void }) {
                     />
                 )}
             </div>
-            <Button className="self-end border-2 border-decoration" variant="secondary" onClick={onNext}>
+            <Button 
+                className="self-end border-2 border-decoration" 
+                variant="secondary" 
+                onClick={onNext}
+                disabled={!selectedShippingOption}
+            >
                 Avançar
             </Button>
         </div>
