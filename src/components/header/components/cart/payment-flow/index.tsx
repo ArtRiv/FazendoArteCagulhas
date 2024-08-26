238111ffa9ecb0403ca133de5a1199106d40d755
ArtRiv <arthurfelaco707@gmail.com>
2024-08-26 17:41:17 -0300

    feat: add authentication checking to buy something

--- src/components/header/components/cart/payment-flow/index.tsx
+++ src/components/header/components/cart/payment-flow/index.tsx
@@ -4,9 +4,14 @@ import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import { BreadcrumbResponsive } from './address-step/breadcrumb';
 import PaymentStep from './payment-step';
+import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
+import { usePathname, useRouter } from 'next/navigation';
 
 export const CheckoutFlow = () => {
     const [currentStep, setCurrentStep] = useState(1);
+    const { isAuthenticated } = useKindeBrowserClient();
+    const router = useRouter();
+    const redirectPath = usePathname();
 
     const handleNextStep = () => {
         setCurrentStep((prevStep) => prevStep + 1);
@@ -23,11 +28,25 @@ export const CheckoutFlow = () => {
         }
     };
 
+    const handleAuthentication = () => {
+        if(!isAuthenticated) {
+            router.push(`/api/auth/login?post_login_redirect_url=${redirectPath}`)
+        }
+        return;
+    }
+
     return (
         <Dialog>
-            <DialogTrigger asChild>
-                <Button variant="outline">Pagar</Button>
-            </DialogTrigger>
+            {isAuthenticated &&
+                <DialogTrigger asChild>
+                    <Button variant="outline">Pagar</Button>
+                </DialogTrigger>
+            }
+            {!isAuthenticated &&
+                <Button onClick={handleAuthentication} variant="outline">
+                    Pagar
+                </Button>
+            }
             <DialogContent className="sm:max-w-[425px] md:max-w-[625px] lg:max-w-[1125px] overflow-y-scroll h-[95%]">
                 <BreadcrumbResponsive />
                 {renderStep()}
