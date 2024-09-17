2b8066da342468a3e5c23e92882100cf5d40afbd
ArtRiv <arthurfelaco707@gmail.com>
2024-09-17 16:19:06 -0300

    feat: add state sharing for routing logic

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,14 @@
+"use client"
+import { useState } from "react";
 import { Dashboard } from "./components/dashboard/dashboard";
 import { Sidebar } from "./components/sidebar/sidebar";
 
-export default async function DashboardPage() {
-
+export default function DashboardPage() {
+  const [selectedRoute, setSelectedRoute] = useState("Analytics");
   return (
-    <main className="grid gap-4 p-4 grid-cols-[220px,_1fr]">
-        <Sidebar />
-        <Dashboard />
+    <main className="grid gap-4 p-4 grid-cols-[220px,_1fr] relative w-screen h-full">
+        <Sidebar selectedRoute={selectedRoute} setSelectedRoute={setSelectedRoute}/>
+        <Dashboard selectedRoute={selectedRoute} />
     </main>
   )
 }
