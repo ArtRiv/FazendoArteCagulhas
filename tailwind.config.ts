45c0881e237f315c7350ae093cc676fba6ad4c9a
ArtRiv <arthurfelaco707@gmail.com>
2024-09-17 16:16:39 -0300

    design: add all shadcn variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -38,14 +38,40 @@ const config: Config = {
         'decoration-indigo': "var(--decoration-indigo)",
         'decoration-indigo-background-20': "var(--decoration-indigo-background-20)",
         'decoration-indigo-background-10': "var(--decoration-indigo-background-10)",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
         },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
         destructive: {
           DEFAULT: "hsl(var(--destructive))",
           foreground: "hsl(var(--destructive-foreground))",
-        }
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        
       },
       fontSize: {
         'smallest': "var(--font-size-smallest)",
@@ -98,7 +124,7 @@ const config: Config = {
       }
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
   darkMode: "class",
 };
 
