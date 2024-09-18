541bb05c879fc4e58528f1dcc09c2980c7509299
ArtRiv <arthurfelaco707@gmail.com>
2024-09-17 22:40:20 -0300

    feat: add dashboard page type

--- src/app/dashboard/components/dashboard/dashboard.tsx
+++ src/app/dashboard/components/dashboard/dashboard.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { TopBar } from "./top-bar";
-import { Grid } from "./grid";
+import { TopBar } from "./top-bar/top-bar";
+import { Grid } from "./grid/grid";
 
-export const Dashboard = () => {
+interface DashbarInterface {
+  selectedRoute: string,
+};
+
+export const Dashboard = ({ selectedRoute }: DashbarInterface) => {
   return (
-    <div className="bg-white rounded-lg pb-4 shadow">
+    <div className="bg-white rounded-lg pb-4 shadow absolute top-3 left-56 w-10/12">
       <TopBar />
       <Grid />
     </div>
