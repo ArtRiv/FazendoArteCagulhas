e44e92dad1bf063756a816e0b32bdd5e9fe2de22
ArtDev <89361400+ArtRiv@users.noreply.github.com>
2024-09-19 23:04:14 +0000

    design: remove side padding

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -9,8 +9,8 @@ export default async function DashboardLayout({
 }: DashboardLayourProps) {
 
     return (
-        <div className="text-stone-950 bg-stone-100 h-screen flex flex-col">
-            <main className="grid gap-4 p-4 grid-cols-[220px,_1fr] relative w-screen h-full">
+        <div className="text-stone-950 bg-stone-100 py-2 h-screen flex flex-col">
+            <main className="grid gap-4 grid-cols-[220px,_1fr] relative w-screen h-full">
                 <Sidebar />
                 {children}
             </main>
